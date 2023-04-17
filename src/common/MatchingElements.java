277d178baead1bb41a581c63ce91c3fcc9267abd
Nikhil Bhole <nrbhole21@gmail.com>
2023-04-17 18:40:06 +0530

    Update MatchingElements.java

--- src/common/MatchingElements.java
+++ src/common/MatchingElements.java
@@ -3,12 +3,13 @@ package common;
 import java.util.HashSet;
 import java.util.Set;
 
+//Program to find out matching elements from 2 arrays
 public class MatchingElements {
 
     public static void main(String[] args) {
         int[] a = {1,2,3};
         int[] b = {4,5,6,1,2};
-
+        
         Set<Integer> set = new HashSet<>();
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < b.length; j++) {
